b5f9ff7d22b8c608db3745fa6b9966b182e7d0e2
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2016-06-14 14:49:04 +0200

    ggh

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var request = require('request');
+var goodGuy = require('good-guy-http')({maxRetries: 3});
 
 var app = express();
 
@@ -19,29 +19,26 @@ app.set('view engine', 'hjs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/book/:isbn', function(req, res, next) {
-  request('https://book-catalog-proxy.herokuapp.com/book?isbn=' + req.params.isbn, function (error, response, body) {
-    if (error) {
-      next(error);
-    }
+app.get('/book/:isbn', function (req, res, next) {
+    goodGuy('https://book-catalog-proxy.herokuapp.com/book?isbn=' + req.params.isbn).then(function (response) {
+        var body = JSON.parse(response.body);
 
-    var body = JSON.parse(body);
-    var title = body.items[0].volumeInfo.title;
-    var cover = body.items[0].volumeInfo.imageLinks.thumbnail;
+        var title = body.items[0].volumeInfo.title;
+        var cover = body.items[0].volumeInfo.imageLinks.thumbnail;
 
-    res.render('book', {title: title, cover: cover});
-  });
+        res.render('book', {title: title, cover: cover});
+    }).catch(next);
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -49,23 +46,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
